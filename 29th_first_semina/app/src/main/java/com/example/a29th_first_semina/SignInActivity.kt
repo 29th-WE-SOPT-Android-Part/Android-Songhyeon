51ed445c2c59a21358bee8c73d90139a38c27658
ssonghyun101 <songhyun0868@gmail.com>
2021-10-07 04:35:59 +0900

    [FEAT] complete signin activity

--- 29th_first_semina/app/src/main/java/com/example/a29th_first_semina/SignInActivity.kt
+++ 29th_first_semina/app/src/main/java/com/example/a29th_first_semina/SignInActivity.kt
@@ -1,12 +1,14 @@
 package com.example.a29th_first_semina
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.example.a29th_first_semina.databinding.ActivitySigninBinding
 
 class SignInActivity : AppCompatActivity() {
-    private lateinit var binding : ActivitySigninBinding
+    private lateinit var binding: ActivitySigninBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySigninBinding.inflate(layoutInflater)
@@ -15,11 +17,41 @@ class SignInActivity : AppCompatActivity() {
         initButtonClickEvent()
 
     }
-    private fun initButtonClickEvent(){
-        binding.btnLogin.setOnClickListener{
-            val user_id : String = binding.etId.toString()
-            val user_pwd : String = binding.etPwd.toString()
+
+    private fun initButtonClickEvent() {
+
+        clickLoginEvent()
+        clickSignUpEvent()
+    }
+
+    private fun moveHomeActivity() {
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun moveSignUpActivity(){
+        val signupintent = Intent(this,SignUpActivity::class.java)
+        startActivity(signupintent)
+
+    }
+
+    private fun clickLoginEvent(){
+        binding.btnLogin.setOnClickListener {
+            val user_id: String = binding.etId.text.toString()
+            val user_pwd: String = binding.etPwd.text.toString()
+            if (user_id.isEmpty() || user_pwd.isEmpty()) {
+                Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
+            } else {
+                moveHomeActivity()
+            }
 
         }
+    }
+
+    private fun clickSignUpEvent(){
+        binding.btnSignin.setOnClickListener{
+            moveSignUpActivity()
+        }
+
     }
 }
---
+++ 29th_first_semina/app/src/main/java/com/example/a29th_first_semina/SignUpActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a29th_first_semina
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
