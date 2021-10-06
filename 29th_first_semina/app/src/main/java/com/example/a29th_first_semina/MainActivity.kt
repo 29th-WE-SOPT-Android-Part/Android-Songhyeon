541782143352217e309d1566d780c615c150bb4c
ssonghyun101 <songhyun0868@gmail.com>
2021-10-07 02:14:48 +0900

    [ADD] add test log

---
+++ 29th_first_semina/app/src/main/java/com/example/a29th_first_semina/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.a29th_first_semina
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d("시작","시작")
+    }
+}
