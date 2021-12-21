09d64cf52b2bde8b56b2b414068d5a43ee5c3869
ssonghyun101 <songhyun0868@gmail.com>
2021-12-22 03:08:48 +0900

    [FEAT] signup network

--- 29th_first_semina/app/src/main/java/com/example/a29th_first_semina/ui/view/sigin/view/SignUpActivity.kt
+++ 29th_first_semina/app/src/main/java/com/example/a29th_first_semina/ui/view/sigin/view/SignUpActivity.kt
@@ -2,8 +2,15 @@ package com.example.a29th_first_semina.ui.view.sigin.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import com.example.a29th_first_semina.databinding.ActivitySignUpBinding
+import com.example.a29th_first_semina.ui.view.sigin.ServiceCreater
+import com.example.a29th_first_semina.ui.view.sigin.data.RequsetSignUpdata
+import com.example.a29th_first_semina.ui.view.sigin.data.ResponseSignUpData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignUpBinding
@@ -31,7 +38,7 @@ class SignUpActivity : AppCompatActivity() {
                         .show()
                 }
                 else {
-                    finish()
+                    initNetwork()
                 }
             }
         }
@@ -40,6 +47,34 @@ class SignUpActivity : AppCompatActivity() {
     }
 
     private fun initNetwork() {
+        val requsetSignupData = RequsetSignUpdata(
+            email = binding.etId.text.toString(),
+            password =  binding.etPwd.toString(),
+            name = binding.etName.toString()
+        )
+
+        val call : Call<ResponseSignUpData> = ServiceCreater.service.postSignup(requsetSignupData)
+
+        call.enqueue(object : Callback<ResponseSignUpData>{
+            override fun onResponse(
+                call: Call<ResponseSignUpData>,
+                response: Response<ResponseSignUpData>
+            ) {
+                if(response.isSuccessful){
+                    val data = response.body()?.data
+                    Log.d("서버통신 상태", response.body()?.status.toString())
+                    finish()
+                }
+
+                else{
+                    Log.d("서버통신 상태", response.body()?.status.toString())
+                }
+            }
+
+            override fun onFailure(call: Call<ResponseSignUpData>, t: Throwable) {
+                Log.d("서버통신 상태", "실패")
+            }
+        })
 
 
     }
