bd96706805da1aec0cc28eb56c3b55f776423023
ssonghyun101 <songhyun0868@gmail.com>
2022-01-01 21:08:57 +0900

    [FEAT] 아이디 && 비번 $$ 이름 셋 다 입력 시 회원가입 버튼 활성화

--- 29th_first_semina/app/src/main/java/com/example/a29th_first_semina/ui/view/sigin/view/SignUpActivity.kt
+++ 29th_first_semina/app/src/main/java/com/example/a29th_first_semina/ui/view/sigin/view/SignUpActivity.kt
@@ -2,6 +2,8 @@ package com.example.a29th_first_semina.ui.view.sigin.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.widget.Toast
 import com.example.a29th_first_semina.databinding.ActivitySignUpBinding
@@ -19,6 +21,8 @@ class SignUpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        checkSignUpBtn()
+        enableSignUpBtn()
         initButtonClick()
     }
 
@@ -34,10 +38,9 @@ class SignUpActivity : AppCompatActivity() {
             val userPwd = etPwd.text
 
             btnSignup.setOnClickListener {
-                if (userName.isEmpty() || userId.isEmpty() || userPwd.isEmpty() ){
+                if (userName.isEmpty() || userId.isEmpty() || userPwd.isEmpty()) {
                     makeToast("입력되지 않은 정보가 있습니다")
-                }
-                else {
+                } else {
                     initNetwork()
                 }
             }
@@ -49,24 +52,22 @@ class SignUpActivity : AppCompatActivity() {
     private fun initNetwork() {
         val requsetSignupData = RequsetSignUpdata(
             email = binding.etId.text.toString(),
-            password =  binding.etPwd.toString(),
+            password = binding.etPwd.toString(),
             name = binding.etName.toString()
         )
 
-        val call : Call<ResponseSignUpData> = ServiceCreater.service.postSignup(requsetSignupData)
+        val call: Call<ResponseSignUpData> = ServiceCreater.service.postSignup(requsetSignupData)
 
-        call.enqueue(object : Callback<ResponseSignUpData>{
+        call.enqueue(object : Callback<ResponseSignUpData> {
             override fun onResponse(
                 call: Call<ResponseSignUpData>,
                 response: Response<ResponseSignUpData>
             ) {
-                if(response.isSuccessful){
+                if (response.isSuccessful) {
                     val data = response.body()?.data
                     Log.d("서버통신 상태", response.body()?.status.toString())
                     finish()
-                }
-
-                else{
+                } else {
                     Log.d("서버통신 상태", response.body()?.status.toString())
                 }
             }
@@ -75,7 +76,66 @@ class SignUpActivity : AppCompatActivity() {
                 Log.d("서버통신 상태", "실패")
             }
         })
+    }
 
+
+    private fun hasUserName() = binding.etName.text.isEmpty()
+    private fun hasUserId() = binding.etId.text.isEmpty()
+    private fun hasUserPwd() = binding.etPwd.text.isEmpty()
+
+    private fun enableSignUpBtn() {
+        binding.btnSignup.isEnabled = !hasUserId() && !hasUserName() && !hasUserPwd()
+    }
+
+    private fun checkSignUpBtn() {
+        binding.etName.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                enableSignUpBtn()
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                binding.btnSignup.isEnabled = false
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                enableSignUpBtn()
+
+            }
+        })
+
+        binding.etId.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                enableSignUpBtn()
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                binding.btnSignup.isEnabled = false
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                enableSignUpBtn()
+
+            }
+        })
+
+        binding.etPwd.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                enableSignUpBtn()
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                binding.btnSignup.isEnabled = false
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                enableSignUpBtn()
+
+            }
+        })
 
     }
+
 }
